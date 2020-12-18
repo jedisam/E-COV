7a63abb2cac6c57d7c23f853704852bdcee9cf3f
Yididya Samuel <yidisam18@gmail.com>
2020-12-17 22:01:44 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template, Response
+from werkzeug.utils import secure_filename
+from myCamera import VideoCamera
+# from gevent.pywsgi import WSGIServer
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/camera', methods=['POST'])
+def camera():
+    return 'HI'
+
+
+def gen(camera):
+    while True:
+        data = camera.get_frame()
+
+        frame = data[0]
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    # port = int(os.environ.get("PORT", 33507))
+    app.run(debug=True)
